65c021d59e1186d04a4b5ccca33b24dceadba1de
Caio <caioymc@live.com>
2019-10-21 12:13:04 +0100

    Use state to render tasks

--- src/App.js
+++ src/App.js
@@ -5,6 +5,23 @@ import Item from "./Item";
 import "./App.css";
 
 class App extends React.Component {
+  state = {
+    tasks: [
+      {text: "walk the cat", completed: true, date: "2019-10-16"},
+      {text: "buy oats", completed: false, date: "2019-10-17"},
+      {text: "learn React", completed: false, date: "2019-10-18"},
+      {text: "wash slippers", completed: true, date: "2019-10-20"},
+      {text: "de-flea the cat", completed: false, date: "2019-10-30"},
+      {text: "create instagram account for cat", completed: true, date: "2019-11-06"}
+    ]
+  }
+  //Function to update the tasks with a new task
+  addTask = (taskText) => {
+    console.log(taskText);
+    //Create a new task with default completed and date propeties
+
+    //Add that task to the state
+  }
   render() {
     return (
       <div className="container">
@@ -15,13 +32,11 @@ class App extends React.Component {
             <AddItem />
           </div>
           <div className="col-12 col-lg-6">
-            <ItemCount count={5} />
+            <ItemCount count={this.state.tasks.length} />
             <ul id="itemList">
-              <Item text="walk the cat" completed={true} />
-              <Item text="buy oats" completed={false} />
-              <Item text="learn React" completed={false} />
-              <Item text="wash slippers" completed={false} />
-              <Item text="de-flea the cat" completed={true} />
+              {this.state.tasks.map(item => {
+                return <Item text={item.text} completed={item.completed} date={item.date} />
+              })}
             </ul>
           </div>
         </div>
