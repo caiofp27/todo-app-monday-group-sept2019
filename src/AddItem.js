2e63e595498121da26fe9cbbf19d19825ef5216c
Caio <caioymc@live.com>
2019-10-21 17:28:17 +0100

    Implement addItem

--- src/AddItem.js
+++ src/AddItem.js
@@ -2,6 +2,21 @@ import React from "react";
 import "./AddItem.css";
 
 class AddItem extends React.Component {
+  //Initial state
+  state = {
+    newTaskText: ""
+  }
+  updateTaskText = (event) => {
+    this.setState({
+      newTaskText: event.target.value
+    });
+  }
+  handleClick = () => {
+    this.props.addTaskFunc(this.state.newTaskText);
+    this.setState({
+      newTaskText: ""
+    });
+  }
   render() {
     // JSX is NOT HTML
     return (
@@ -9,11 +24,13 @@ class AddItem extends React.Component {
         <input
           id="addItemInput"
           type="text"
-          class="form-control"
+          className="form-control"
           placeholder="E.g. take out bins"
+          value={this.state.newTaskText}
+          onChange={this.updateTaskText}
         />
         <div className="mt-3 mb-3">
-          <button id="addButton" className="btn">
+          <button id="addButton" className="btn" onClick={this.handleClick}>
             Add to list
           </button>
         </div>
@@ -23,3 +40,4 @@ class AddItem extends React.Component {
 }
 
 export default AddItem;
+
--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import uuid from "uuid/v4";
 import AddItem from "./AddItem";
 import ItemCount from "./ItemCount";
 import Item from "./Item";
@@ -7,20 +8,31 @@ import "./App.css";
 class App extends React.Component {
   state = {
     tasks: [
-      {text: "walk the cat", completed: true, date: "2019-10-16"},
-      {text: "buy oats", completed: false, date: "2019-10-17"},
-      {text: "learn React", completed: false, date: "2019-10-18"},
-      {text: "wash slippers", completed: true, date: "2019-10-20"},
-      {text: "de-flea the cat", completed: false, date: "2019-10-30"},
-      {text: "create instagram account for cat", completed: true, date: "2019-11-06"}
+      {text: "walk the cat", completed: true, date: "2019-10-16", id: uuid()},
+      {text: "buy oats", completed: false, date: "2019-10-17", id: uuid()},
+      {text: "learn React", completed: false, date: "2019-10-18", id: uuid()},
+      {text: "wash slippers", completed: true, date: "2019-10-20", id: uuid()},
+      {text: "de-flea the cat", completed: false, date: "2019-10-30", id: uuid()},
+      {text: "create instagram account for cat", completed: true, date: "2019-11-06", id: uuid()}
     ]
   }
   //Function to update the tasks with a new task
   addTask = (taskText) => {
     console.log(taskText);
     //Create a new task with default completed and date propeties
-
+    const newTask = {
+      text: taskText,
+      completed: false,
+      date: "2019-10-21",
+      id: uuid()
+    };
     //Add that task to the state
+    //Never do this.state.tasks.push(item) and access state directly 
+    const tasksCopy = this.state.tasks.slice();
+    tasksCopy.push(newTask);
+    this.setState({
+      tasks: tasksCopy
+    });
   }
   render() {
     return (
@@ -29,13 +41,13 @@ class App extends React.Component {
         <h3>Things I need to do very soon.</h3>
         <div className="row">
           <div className="col-12 col-lg-6">
-            <AddItem />
+            <AddItem addTaskFunc={this.addTask} />
           </div>
           <div className="col-12 col-lg-6">
             <ItemCount count={this.state.tasks.length} />
             <ul id="itemList">
               {this.state.tasks.map(item => {
-                return <Item text={item.text} completed={item.completed} date={item.date} />
+                return <Item key={item.id} text={item.text} completed={item.completed} date={item.date} />
               })}
             </ul>
           </div>
